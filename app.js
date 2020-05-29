f7d4fae1a95a3f0565f29ecda4381d6bdc08617c
Akeem Adkins <adkinsakeem@gmail.com>
2020-05-29 01:41:33 -0400

    Adding the body of the site. Also, adding the layout of the checkboxes.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 
 const app = express();
 
-let posts = [];
+const categories  = ['Adults','Animals','Art','Career','Celebration','Children','Cold','Confidence','Controversy','Criticism','Current Events','Education','Emotions','Fall','Family','Favorite','Fiction','Finish/Fill in the blank','Food','Friends','Happy','Hate','Health','History','Hobby','Holiday','Home','Hot','How-To','Indoor','Law','Literature','Love','Memories','Mental Health','Money','Motivation/Inspiration','Music','Nature','Non-Fiction','Opinion','Outdoor','People','Political','Pop Culture','Recreation','Religion','Religion','Sad','Self','Sports','Spring','Summer','Task','Technology','Time','Vacation','Vintage','Warm','Winter'];
 
 
 app.set('view engine', 'ejs');
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-res.render("home", {postsInput: posts});
+res.render("home", {topicCategories: categories});
 
 }); 
 
