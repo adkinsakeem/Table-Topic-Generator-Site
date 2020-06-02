aedfb4e9ee25677c3fcb45dcfc2e99dd109ec21c
Akeem Adkins <adkinsakeem@gmail.com>
2020-06-02 18:05:41 -0400

    The search categories have been added. Also the results page is added, but it is blank. You can also choose the number of results you would like.

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const https = require("https");
 
 const app = express();
 
 const categories  = ['Adults','Animals','Art','Career','Celebration','Children','Cold','Confidence','Controversy','Criticism','Current Events','Education','Emotions','Fall','Family','Favorite','Fiction','Finish/Fill in the blank','Food','Friends','Happy','Hate','Health','History','Hobby','Holiday','Home','Hot','How-To','Indoor','Law','Literature','Love','Memories','Mental Health','Money','Motivation/Inspiration','Music','Nature','Non-Fiction','Opinion','Outdoor','People','Political','Pop Culture','Recreation','Religion','Religion','Sad','Self','Sports','Spring','Summer','Task','Technology','Time','Vacation','Vintage','Warm','Winter'];
-
+let ttJSON;
 
 app.set('view engine', 'ejs');
 
@@ -21,6 +22,56 @@ res.render("home", {topicCategories: categories});
 
 }); 
 
+app.post("/", function(req, res){
+	console.log("test");
+	var checkedBoxes = [];
+	var checkedJSON = req.body;
+	var TTArray = [];
+	var tableTopicsAPI = "https://toastmasterstabletopicsapi.herokuapp.com/randomTopic?count=";
+	for(var x=0;x<categories.length;x++){
+		if(checkedJSON[categories[x]] === "on"){
+			checkedBoxes.push(categories[x]);
+		}
+	}
+	tableTopicsAPI += req.body.tableTopicNummberSpinner;
+		for(var y=0;y<checkedBoxes;y++){
+	tableTopicsAPI	+="&category1="+checkedBoxes[y];
+		}
+		https.get(tableTopicsAPI, function(response){
+
+			response.on("data", function(data){
+			ttJSON = (JSON.parse(data).Topics);
+			//console.log(ttJSON[0].Table_Topic);
+			//console.log(ttJSON[1].Table_Topic);
+
+			//res.send("/results"+ttJSON);
+						res.redirect("/results");
+
+			});
+
+
+		});
+
+
+
+
+		/*request.open('GET', tableTopicsAPI, true);
+		request.onload = function(){
+			var data = JSON.parse(this.response);
+			if(request.status == 200){
+				data.forEach(TTList, function(){
+					TTArray.push(TTList);
+				});
+			}
+		}
+		request.send();
+		res.redirect("/results", TTArray);*/
+
+});
+
+app.get("/results", function(req, res){
+	res.render("results", {TTArrayList: req.TTJSON});
+});
 
 
 
