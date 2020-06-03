b40f043e19e06e130591106318c7991a0844e4d0
Akeem Adkins <adkinsakeem@gmail.com>
2020-06-03 00:33:47 -0400

    Results page now yields results. Also has a button to go to previous page.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 const categories  = ['Adults','Animals','Art','Career','Celebration','Children','Cold','Confidence','Controversy','Criticism','Current Events','Education','Emotions','Fall','Family','Favorite','Fiction','Finish/Fill in the blank','Food','Friends','Happy','Hate','Health','History','Hobby','Holiday','Home','Hot','How-To','Indoor','Law','Literature','Love','Memories','Mental Health','Money','Motivation/Inspiration','Music','Nature','Non-Fiction','Opinion','Outdoor','People','Political','Pop Culture','Recreation','Religion','Religion','Sad','Self','Sports','Spring','Summer','Task','Technology','Time','Vacation','Vintage','Warm','Winter'];
 let ttJSON;
+let resultsCount;
 
 app.set('view engine', 'ejs');
 
@@ -23,17 +24,17 @@ res.render("home", {topicCategories: categories});
 }); 
 
 app.post("/", function(req, res){
-	console.log("test");
 	var checkedBoxes = [];
 	var checkedJSON = req.body;
 	var TTArray = [];
 	var tableTopicsAPI = "https://toastmasterstabletopicsapi.herokuapp.com/randomTopic?count=";
+	resultsCount = req.body.tableTopicNumberSpinner;
 	for(var x=0;x<categories.length;x++){
 		if(checkedJSON[categories[x]] === "on"){
 			checkedBoxes.push(categories[x]);
 		}
 	}
-	tableTopicsAPI += req.body.tableTopicNummberSpinner;
+	tableTopicsAPI += req.body.tableTopicNumberSpinner;
 		for(var y=0;y<checkedBoxes;y++){
 	tableTopicsAPI	+="&category1="+checkedBoxes[y];
 		}
@@ -41,11 +42,14 @@ app.post("/", function(req, res){
 
 			response.on("data", function(data){
 			ttJSON = (JSON.parse(data).Topics);
+			//res.send(JSON.stringify(res));
+			//console.log(JSON.parse(data));
+			//console.log(ttJSON[0]);
+			//console.log(ttJSON[1]);
 			//console.log(ttJSON[0].Table_Topic);
-			//console.log(ttJSON[1].Table_Topic);
 
 			//res.send("/results"+ttJSON);
-						res.redirect("/results");
+			res.redirect("/results");
 
 			});
 
@@ -70,7 +74,8 @@ app.post("/", function(req, res){
 });
 
 app.get("/results", function(req, res){
-	res.render("results", {TTArrayList: req.TTJSON});
+	console.log(ttJSON[0].Table_Topic);
+	res.render("results", {TTArrayList: ttJSON, ttCounter: resultsCount});
 });
 
 
